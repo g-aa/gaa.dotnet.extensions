9731854077b3f428ca8980c20a3a4e7d33de8679
Georgievskiy Andrey Aleksandrovich <Georgievskiy-Andrey@mail.ru>
2026-09-07 21:21:10 +0300

    Обновление .benchmark.sh

--- build/.benchmark.sh
+++ build/.benchmark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd ..
-dotnet run --project ./benchmark/Gaa.Extensions.Benchmark --configuration Release --filter '*' --join
+dotnet run --project ./benchmark/Gaa.Extensions.Benchmark --configuration Release --filter 'Gaa.Extensions.Benchmark.Observer*' --join
 cat ./BenchmarkDotNet.Artifacts/results/*.md
